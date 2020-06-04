02a923d2066ca68f411e0cac03d4f5ff66daed33
Kristian <kristian.roth95@gmail.com>
2020-06-04 18:41:31 +0300

    Working dispatch to github

--- wp-headless-fetch-frontend.php
+++ wp-headless-fetch-frontend.php
@@ -13,13 +13,13 @@ add_action( 'save_post', 'send_repository_dispatch' );
 
 function send_repository_dispatch($post_id) {
 
-  if (wp_is_post_revision($post_id)) {
+  if (wp_is_post_revision($post_id) || wp_is_post_autosave($post_id)) {
     return;
   }
 
-  $ACCESS_TOKEN = "637b21ebb444274335765991ea218d8601e98a70";
+  $ACCESS_TOKEN = GITHUB_ACCESS_TOKEN;
 
-  $response = wp_remote_post('https://api.github.com/repos/deltary/website/dispatches/', array(
+  $response = wp_remote_post('https://api.github.com/repos/deltary/website/dispatches', array(
       'method'    => 'POST',
       'headers'   => array(
         'User-Agent'      => 'https://delta.utu.fi',
@@ -27,17 +27,14 @@ function send_repository_dispatch($post_id) {
         'Accept'          => 'application/vnd.github.everest-preview+json',
         'Content-Type'    => 'application/json'
       ),
-      'body'      => array(
-        'event_type'      => 'repository_dispatch',
-      ),
+      'body'      => json_encode(array(
+        'event_type'      => 'save_post',
+      )),
     )
   );
 
   if (is_wp_error($response)) {
-    error_log("Failed");
+    error_log("Failed to dispatch github action");
     error_log($response->get_error_message());
-  } else {
-    error_log("Success");
-    error_log(print_r($response, true));
-  }
+  } 
 }
