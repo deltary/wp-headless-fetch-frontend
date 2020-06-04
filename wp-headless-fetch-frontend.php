c2849121e5b9647ff8141e2cc9dde16ca1eba59a
Kristian <kristian.roth95@gmail.com>
2020-06-04 17:21:29 +0300

    Removed python code and started making php action dispatch

--- wp-headless-fetch-frontend.php
+++ wp-headless-fetch-frontend.php
@@ -8,4 +8,35 @@
  * Author URI: https://github.com/kovipu
  */
 
-add_action( 'save_post', function () { exec("python3 fetch.py"); } );
+
+add_action( 'save_post', 'send_repository_dispatch' );
+
+function send_repository_dispatch($post_id) {
+
+  if (wp_is_post_revision($post_id)) {
+    return;
+  }
+
+  $ACCESS_TOKEN = "637b21ebb444274335765991ea218d8601e98a70";
+
+  $response = wp_remote_post('https://api.github.com/repos/deltary/website/dispatches', array(
+      'method'    => 'POST',
+      'headers'   => array(
+        'User-Agent'      => 'https://delta.utu.fi',
+        'Authorization'   => 'token ' . $ACCESS_TOKEN,
+        'accept'          => 'application/vnd.github.everest-preview+json'
+      ),
+      'body'      => array(
+        'event_type'      => 'repository_dispatch',
+      ),
+    )
+  );
+
+  if (is_wp_error($response)) {
+    error_log("Failed");
+    error_log($response->get_error_message());
+  } else {
+    error_log("Success");
+    error_log(print_r($response, true));
+  }
+}
